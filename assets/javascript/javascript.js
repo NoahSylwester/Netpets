6122125197fc9c9b01ec0c1fea88a730a818d55a
Kaitlyn Steagall <kaitlynsteagall@gmail.com>
2019-09-07 11:07:54 -0700

    displays for the pages and some bug fixes to background image

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,16 +7,16 @@ var petArr = [];
 var currentPet = 0;
 
 // this constructor creates a new netpet
-var NetPet = function(name, hungerRate) {
+var NetPet = function (name, hungerRate) {
   this.name = name,
-  this.hungerValue = 100;
+    this.hungerValue = 100;
   this.hungerRate = hungerRate;
 
-  this.getHungrier = function() { // add hunger over time
+  this.getHungrier = function () { // add hunger over time
     hungerValue += hungerRate;
   };
 
-  this.feed = function(foodValue) {
+  this.feed = function (foodValue) {
     this.hungerValue += foodValue;
     if (this.hungerValue >= 200) { // limit amount of feeding
       this.hungerValue = 200;
@@ -27,7 +27,7 @@ var NetPet = function(name, hungerRate) {
 }
 
 // this constructor creates food items
-var FoodItem = function(foodValue) {
+var FoodItem = function (foodValue) {
   this.foodValue = foodValue;
 }
 
@@ -39,7 +39,7 @@ var cookie = new FoodItem(10);
 
 
 // feed pet with feed button
-$('#feed-button').on('click', function(event) {
+$('#feed-button').on('click', function (event) {
   event.preventDefault();
   petArr[currentPet].feed(cookie.foodValue);
 });
@@ -49,6 +49,10 @@ $('#petDisplay').hide();
 $('#petLog').hide();
 $('#petStore').hide();
 $('#miniGames').hide();
+$('#fruit').hide();
+$('#chase').hide();
+$('#race').hide();
+$('#follow').hide();
 
 
 
@@ -61,81 +65,140 @@ $('#miniGames').hide();
 // )
 
 // pet selection
-  $("#pet1").click(function (){
-    $("#petChoose").hide();
-    $('#petName').show();
-    petId = "Doux";
-    $(".pet-sprite").attr({"src":"./assets/gifs/DinoSprites_doux.gif"});
-    petArr.push(new NetPet(petName, 100));
-  });
-  $("#pet2").click(function (){
-    $("#petChoose").hide();
-    $('#petName').show();
-    petId = "Mort";
-    $(".pet-sprite").attr({"src":"./assets/gifs/DinoSprites_mort.gif"});
-    petArr.push(new NetPet(petName, 100));
-  });
-  $("#pet3").click(function (){
-    $("#petChoose").hide();
-    $('#petName').show();
-    petId = "Tard";
-    $(".pet-sprite").attr({"src":"./assets/gifs/DinoSprites_tard.gif"});
-    petArr.push(new NetPet(petName, 100));
-  });
-  $("#pet4").click(function (){
-    $("#petChoose").hide();
-    $('#petName').show();
-    petId = "Vita";
-    $(".pet-sprite").attr({"src":"./assets/gifs/DinoSprites_vita.gif"});
-    petArr.push(new NetPet(petName, 100));
-  });
+$("#pet1").click(function () {
+  $("#petChoose").hide();
+  $('#petName').show();
+  petId = "Doux";
+  $(".pet-sprite").attr({ "src": "./assets/gifs/DinoSprites_doux.gif" });
+  petArr.push(new NetPet(petName, 100));
+});
+$("#pet2").click(function () {
+  $("#petChoose").hide();
+  $('#petName').show();
+  petId = "Mort";
+  $(".pet-sprite").attr({ "src": "./assets/gifs/DinoSprites_mort.gif" });
+  petArr.push(new NetPet(petName, 100));
+});
+$("#pet3").click(function () {
+  $("#petChoose").hide();
+  $('#petName').show();
+  petId = "Tard";
+  $(".pet-sprite").attr({ "src": "./assets/gifs/DinoSprites_tard.gif" });
+  petArr.push(new NetPet(petName, 100));
+});
+$("#pet4").click(function () {
+  $("#petChoose").hide();
+  $('#petName').show();
+  petId = "Vita";
+  $(".pet-sprite").attr({ "src": "./assets/gifs/DinoSprites_vita.gif" });
+  petArr.push(new NetPet(petName, 100));
+});
 
-  
 
-  $('#save-name').on('click', function () {
-    petName = $('#pet-name-input').val().trim();
-    if (petName === "") {
-      petName = petId;
-    }
-    $('#pet-home-link').text(petName);
-    $("#petName").hide();
-    $("#petDisplay").show();
-  })
 
-  $("#store").click(function (){
-    $("#petStore").show();
-    $('#petDisplay').hide();
-    $('#miniGames').hide();
-    $('#petLog').hide();
-  });
+$('#save-name').on('click', function () {
+  petName = $('#pet-name-input').val().trim();
+  if (petName === "") {
+    petName = petId;
+  }
+  $('#pet-home-link').text(petName);
+  $("#petName").hide();
+  $("#petDisplay").show();
+})
 
-  $("#log").click(function (){
-    $("#petStore").hide();
-    $('#petDisplay').hide();
-    $('#miniGames').hide();
-    $('#petLog').show();
-  });
+$("#store").click(function () {
+  $("#petStore").show();
+  $('#petDisplay').hide();
+  $('#miniGames').hide();
+  $('#petLog').hide();
+  $('#fruit').hide();
+  $('#chase').hide();
+  $('#race').hide();
+  $('#follow').hide();
+});
 
-  $("#collect-chippies").click(function (){
-    $("#petStore").hide();
-    $('#petDisplay').hide();
-    $('#miniGames').show();
-    $('#petLog').hide();
-  });
+$("#log").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').hide();
+  $('#miniGames').hide();
+  $('#petLog').show();
+  $('#fruit').hide();
+  $('#chase').hide();
+  $('#race').hide();
+  $('#follow').hide();
+});
 
-  $("#pet-home-link").click(function (){
-    $("#petStore").hide();
-    $('#petDisplay').show();
-    $('#miniGames').hide();
-    $('#petLog').hide();
-  });
+$("#collect-chippies").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').hide();
+  $('#miniGames').show();
+  $('#petLog').hide();
+  $('#fruit').hide();
+  $('#chase').hide();
+  $('#race').hide();
+  $('#follow').hide();
+});
 
+$("#pet-home-link").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').show();
+  $('#miniGames').hide();
+  $('#petLog').hide();
+  $('#fruit').hide();
+  $('#chase').hide();
+  $('#race').hide();
+  $('#follow').hide();
+});
+
+$("#game1").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').hide();
+  $('#miniGames').hide();
+  $('#petLog').hide();
+  $('#fruit').show();
+  $('#chase').hide();
+  $('#race').hide();
+  $('#follow').hide();
+});
+
+$("#game2").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').hide();
+  $('#miniGames').hide();
+  $('#petLog').hide();
+  $('#fruit').hide();
+  $('#chase').show();
+  $('#race').hide();
+  $('#follow').hide();
+});
+
+$("#game3").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').hide();
+  $('#miniGames').hide();
+  $('#petLog').hide();
+  $('#fruit').hide();
+  $('#chase').hide();
+  $('#race').show();
+  $('#follow').hide();
+});
+
+$("#game4").click(function () {
+  $("#petStore").hide();
+  $('#petDisplay').hide();
+  $('#miniGames').hide();
+  $('#petLog').hide();
+  $('#fruit').hide();
+  $('#chase').hide();
+  $('#race').hide();
+  $('#follow').show();
+});
 // weather API calls
 var coordinates = [];
 var weatherCondition = "Clear";
-window.onload = function() {
+window.onload = function () {
   var startPos;
-  var geoSuccess = function(position) {
+  var geoSuccess = function (position) {
     startPos = position;
     coordinates[0] = startPos.coords.latitude;
     coordinates[1] = startPos.coords.longitude;
@@ -155,62 +218,62 @@ window.onload = function() {
   navigator.geolocation.getCurrentPosition(geoSuccess);
 };
 
- 
- //firebase script link
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB2-iC_UGBuJwHLq98-xnOS6Q6izNH5vts",
-    authDomain: "netpet-a7d7a.firebaseapp.com",
-    databaseURL: "https://netpet-a7d7a.firebaseio.com",
-    projectId: "netpet-a7d7a",
-    storageBucket: "",
-    messagingSenderId: "407927560249",
-    appId: "1:407927560249:web:a2c1240e29efb5ec9c95e8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+//firebase script link
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB2-iC_UGBuJwHLq98-xnOS6Q6izNH5vts",
+  authDomain: "netpet-a7d7a.firebaseapp.com",
+  databaseURL: "https://netpet-a7d7a.firebaseio.com",
+  projectId: "netpet-a7d7a",
+  storageBucket: "",
+  messagingSenderId: "407927560249",
+  appId: "1:407927560249:web:a2c1240e29efb5ec9c95e8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
 //time stamps for the activity log 
 
-$('#feed-button').on('click' , function(event){
+$('#feed-button').on('click', function (event) {
   event.preventDefault();
   var currentTime = new Date();
-  database.ref().update({currentTime:currentTime})
+  database.ref().update({ currentTime: currentTime })
 });
-$('#play-button').on('click' , function(event){
+$('#play-button').on('click', function (event) {
   event.preventDefault();
   var currentTime = new Date();
-  database.ref().update({currentTime:currentTime})
+  database.ref().update({ currentTime: currentTime })
 });
-$('#potty-button').on('click' , function(event){
+$('#potty-button').on('click', function (event) {
   event.preventDefault();
   var currentTime = new Date();
-  database.ref().update({currentTime:currentTime})
+  database.ref().update({ currentTime: currentTime })
 });
-$('#love-button').on('click' , function(event){
+$('#love-button').on('click', function (event) {
   event.preventDefault();
   var currentTime = new Date();
-  database.ref().update({currentTime:currentTime})
+  database.ref().update({ currentTime: currentTime })
 });
-$('#sleep-button').on('click' , function(event){
+$('#sleep-button').on('click', function (event) {
   event.preventDefault();
   var currentTime = new Date();
-  database.ref().update({currentTime:currentTime})
+  database.ref().update({ currentTime: currentTime })
 });
 
 //current time stamps append to activity log in the table
-$("#feed-button").on("click" , function(table){
-var today = Date();
-var getTime = today.toString()
-var type = "";
-document.getElementById('time').innerHTML = getTime;
+$("#feed-button").on("click", function (table) {
+  var today = Date();
+  var getTime = today.toString()
+  var type = "";
+  document.getElementById('time').innerHTML = getTime;
 
-if( type = 'Feed'){
- document.getElementById('type').innerHTML = type;
-}
-// $("#petCareTable").append("<tr>");
-// $("#petCareTable").append("<td>");
+  if (type = 'Feed') {
+    document.getElementById('type').innerHTML = type;
+  }
+  // $("#petCareTable").append("<tr>");
+  // $("#petCareTable").append("<td>");
 });
 
